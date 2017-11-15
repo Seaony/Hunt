a60f161779ef2ce5e15756aefe8dcb13496f36bc
Seaony <seaony@outlook.com>
2017-11-15 11:17:59 +0800

    :tada: fix layouts footer about us

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <ul class="links">
         <li><a href="{{ route('index.friendships') }}" id="Friend-links">Friend links</a></li>
-        <li><a href="{{ route('index.about') }}" id="About-Me">About our</a></li>
+        <li><a href="{{ route('index.about') }}" id="About-Me">About us</a></li>
     </ul>
     <p>Developed By <a href="https://github.com/Seaony" target="_blank">Seaony</a><i class="fa fa-heart"></i>Zrl</p>
 </footer>
